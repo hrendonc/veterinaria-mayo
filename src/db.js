6bfdbedc274c1b6811f4cf7c420c33149cd528c3
hrendonc <flakonet@msn.com>
2022-04-08 16:00:49 -0500

    Add cloud DB

--- src/db.js
+++ src/db.js
@@ -1,12 +1,13 @@
 const db = require('mongoose')
+require('dotenv').config()
 
 db.Promise = global.Promise
 
 async function connect(){
-    const uri = 'mongodb://localhost:27017/vetmayo'
+    const uri = `mongodb+srv://vetmayo:${process.env.PASSWORD}@cluster0.78sjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
     await db.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=>console.log('Conectado a Mongo DB'))
-    .catch(e=>console.log('[Error de conexión] - ', e))
+    .catch(e=>console.log('[Error de conexión DB] - ', e))
 }
 
 module.exports = connect
