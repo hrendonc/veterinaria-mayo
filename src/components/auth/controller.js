708500f5eb85a478f6dce39b31b6cd7673ae03c4
hrchilpo <hrchilpo@gmail.com>
2022-07-16 15:57:26 -0500

    Corregido errores del modulo Auth

--- src/components/auth/controller.js
+++ src/components/auth/controller.js
@@ -6,6 +6,8 @@ require('dotenv').config()
 exports.signUp = async (req, res)=>{
     const {user, pass, email, roles} = req.body
 
+    if(!pass || !user || !email) return res.status(400).json({Message: 'No se recibieron los datos requeridos'})
+
     try {
         const newUser = new User({
             user,
@@ -24,26 +26,26 @@ exports.signUp = async (req, res)=>{
         const savedUser = await newUser.save()
         const token = jwt.sign({id: savedUser._id}, process.env.SECRET, {expiresIn: '1d'})
     
-        console.log(savedUser)
-        res.status(200).json(token)
+        res.status(200).json({token})
 
     } catch (error) {
         console.log(error)
-    }
-    
+    }    
 }
 
 exports.signIn = async (req, res)=>{
-    const userFound = await User.findOne({email: req.body.email}).populate('roles')
+    const {email, pass} = req.body
+    if(!email || !pass) return res.status(400).json({Message: 'No se recibieron los datos esperados'})
+
+    const userFound = await User.findOne({email}).populate('roles')
     
     if(!userFound) return res.status(400).json({message: 'User not found'})
 
-    const matchPass = await User.comparePass(req.body.pass, userFound.pass)
+    const matchPass = await User.comparePass(pass, userFound.pass)
 
     if(!matchPass) return res.status(400).json({token: null, message: 'Invalid password'})
 
     const token = jwt.sign({id: userFound._id}, process.env.SECRET, {expiresIn: '1d'})
 
-    console.log(userFound)
-    res.json({token})
+    res.json({User:userFound.user, token})
 }
